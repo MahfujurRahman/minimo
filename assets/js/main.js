0f023fdcd6acdfacd4b9f8a64c56b758ac91d807
Mahfujur Rahman <mahfujur705@gmail.com>
2018-03-01 11:55:37 +0600

    last commit
    responcive

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,17 @@
         $('[data-toggle="tooltip"]').tooltip();
 
 
+        $(".treger-menu").on("click", function(){
+        	$(".offe-canvar-menu, .canvar-menu-overlay").addClass("active");
+        	return false;
+        })
+
+        $(".menu-close, .canvar-menu-overlay").on("click", function(){
+        	$(".offe-canvar-menu, .canvar-menu-overlay").removeClass("active");
+        })
+
+
+
 
         
 
